0bb9ffa1e8be4e7f0458d2399c6af7dc82ff1593
Nikshit joshi <nikshitjoshi25@gamil.com>
2026-08-26 07:37:30 +0530

    ecom-backend

--- roles.js
+++
@@ -1,6 +0,0 @@
-const ROLES = {USER: 'user', SELLER:'seller', ADMIN: 'admin'};
-const ROLE_LIST = Object.values(roles);
-
-const STGNUP_ROLES = [ROLES.USER, ROLES.SELLER];
-
-module.exports={roles,ROLE_LIST,STGNUP_ROLES};
